b562b2f8c0202febc0c1ded04b0bb2bc6a77613a
James Deane <25thhour+git@gmail.com>
2022-01-17 11:11:58 +1300

    Create 11ty filter to populate the snippet body with an array of the source data (line by line) Fixes #1

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addNunjucksFilter("formatSnippetBody", function (content) {
+    // escape " and create array of lines
+    const snippet = content.replace(/"/g, '\\"').split("\n");
+    // wrap each line in double quotes
+    const formattedSnippet = snippet.map((line, index) => {
+      // don't add a comma after the last line
+      return index === snippet.length - 1 ? `"${line}"` : `"${line}",`;
+    });
+
+    return formattedSnippet.join("\n");
+  });
+};
