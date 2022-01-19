e72bc838fe4d08f66422ceb049939ded4d577d10
James Deane <25thhour+git@gmail.com>
2022-01-20 10:20:55 +1300

    💩 forgot to actually escape the \t !

--- .eleventy.js
+++ .eleventy.js
@@ -3,21 +3,18 @@ module.exports = function (eleventyConfig) {
     // escape " and create array of lines
     const snippet = content.replace(/"/g, '\\"').trim().split("\n");
     const spaceIndent = indent > 0 ? indent : 2;
-    // wrap each line in double quotes
-    const formattedSnippet = snippet.map((line, index) => {
-      // if lines are indented with tabs, no need to transform
-      if (line.charAt(0) === "\t") {
-        return index === snippet.length - 1 ? `"${line}"` : `"${line}",`;
-      }
 
-      // if lines are indented with spaces, transform to tabs
-      // count the number of leading spaces
+    const formattedSnippet = snippet.map((line, index) => {
+      // count the number of leading whitespace characters (tab or spaces)
       const indentCount = line.search(/\S/) !== -1 ? line.search(/\S/) : 0;
-      const newLine = line
-        // remove the leading spaces
-        .trimStart()
-        // add a \t for each tabstop according to indentCount
-        .replace(/^/, "\t".repeat(indentCount / spaceIndent));
+      let newLine = line;
+
+      if (line.charAt(0) === "\t") {
+        newLine = line.replace(/\t/g, "\\t");
+      } else {
+        // add a \t for each space tab according to indentCount
+        newLine = newLine.trimStart().replace(/^/, "\\t".repeat(indentCount / spaceIndent));
+      }
       // don't add a comma after the last line
       return index === snippet.length - 1 ? `"${newLine}"` : `"${newLine}",`;
     });
