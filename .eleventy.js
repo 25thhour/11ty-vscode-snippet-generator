818c4d638146e328192d6467dab6e14a471d4a3d
James Deane <25thhour@gmail.com>
2022-01-20 10:29:58 +1300

    Configurable space indent and conversion to tabs (#5)
    * convert spaces to \t
    * handle tab based snippets
    * allow space indent to be configurable on a per snippet basis
    
    Fixes Handle indentation #4

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,22 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addNunjucksFilter("formatSnippetBody", function (content) {
+  eleventyConfig.addShortcode("formatSnippetBody", function (content, indent) {
     // escape " and create array of lines
-    const snippet = content.replace(/"/g, '\\"').split("\n");
-    // wrap each line in double quotes
+    const snippet = content.replace(/"/g, '\\"').trim().split("\n");
+    const spaceIndent = indent > 0 ? indent : 2;
+
     const formattedSnippet = snippet.map((line, index) => {
+      // count the number of leading whitespace characters (tab or spaces)
+      const indentCount = line.search(/\S/) !== -1 ? line.search(/\S/) : 0;
+      let newLine = line;
+
+      if (line.charAt(0) === "\t") {
+        newLine = line.replace(/\t/g, "\\t");
+      } else {
+        // add a \t for each space tab according to indentCount
+        newLine = newLine.trimStart().replace(/^/, "\\t".repeat(indentCount / spaceIndent));
+      }
       // don't add a comma after the last line
-      return index === snippet.length - 1 ? `"${line}"` : `"${line}",`;
+      return index === snippet.length - 1 ? `"${newLine}"` : `"${newLine}",`;
     });
 
     return formattedSnippet.join("\n");
