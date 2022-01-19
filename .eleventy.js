c50b6a3e0aaa23e3fb03c37029503ac03adf4611
James Deane <25thhour+git@gmail.com>
2022-01-19 15:00:15 +1300

    convert spaces to \t

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,20 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addNunjucksFilter("formatSnippetBody", function (content) {
     // escape " and create array of lines
     const snippet = content.replace(/"/g, '\\"').split("\n");
+    // default indentation
+    // TODO make indent value configurable
+    const indent = 2;
     // wrap each line in double quotes
     const formattedSnippet = snippet.map((line, index) => {
+      // count the number of leading spaces or \t chars
+      const indentCount = line.search(/\S/) !== -1 ? line.search(/\S/) : 0;
+      const newLine = line
+        // remove the leading whitespace
+        .trimStart()
+        // add a \t for each tabstop according to indentCount
+        .replace(/^/, "\t".repeat(indentCount / indent));
       // don't add a comma after the last line
-      return index === snippet.length - 1 ? `"${line}"` : `"${line}",`;
+      return index === snippet.length - 1 ? `"${newLine}"` : `"${newLine}",`;
     });
 
     return formattedSnippet.join("\n");
