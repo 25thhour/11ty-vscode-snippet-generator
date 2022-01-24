0deeb14032c5c0108371339cc9d58651372a87e0
James Deane <25thhour@gmail.com>
2022-01-25 10:45:28 +1300

    compute category (subfolder) and re-add generic 'snippets' tag

--- src/snippets/snippets.11tydata.js
+++ src/snippets/snippets.11tydata.js
@@ -1,7 +1,12 @@
 module.exports = {
   eleventyComputed: {
     indent: (data) => data.spaceIndent,
+    category: (data) => {
+      const stem = data.page.filePathStem
+      return stem.slice(0, stem.lastIndexOf('/'))
+    },
     permalink: (data) => `${data.page.filePathStem}.json`,
   },
+  tags: "snippets",
   layout: "snippet.njk",
 };
